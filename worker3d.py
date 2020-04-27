863df366e1f57c59ba531c369a0d3ed7bc8f5931
Alexey Sizanov <alexey.sizanov@gmail.com>
2020-04-28 00:14:05 +0300

    1) Separation of the optimization and twist in Worker. 2) npz -> pickle

--- worker3d.py
+++ worker3d.py
@@ -1,6 +1,6 @@
 import gc
 from itertools import count
-from typing import Optional
+from typing import Optional, List
 import pickle
 
 import numpy as np
@@ -10,19 +10,23 @@ from tqdm import tqdm
 from system3d import TDSystem3D
 
 class Worker3D:
-    def __init__(self, L, H, c):
-        self.H = H
+    def __init__(self, L, c):
         self.c = c
         self.L = L
 
         self.n_realizations = 0
+        self.n_twists_done = 0
         self.dH1 = []
         self.dH2 = []
         self.chirality = []
-        self.fourier = np.zeros((L, L, H))
 
-    def _make_one_realization(self, lr=0.5, n_steps=3000, device='cpu', progress=False):
-        s = TDSystem3D(L=self.L, H=self.H, c=self.c, device=device)
+        self.spin_inds: List[np.ndarray] = []
+        self.thetas: List[np.ndarray] = []
+        self.phis: List[np.ndarray] = []
+
+
+    def _find_one_realization(self, lr=0.5, n_steps=3000, device='cpu', progress=False):
+        s = TDSystem3D(L=self.L, H=self.L, c=self.c, device=device)
         s.save_init_state()
 
         for i in count(1):
@@ -41,15 +45,9 @@ class Worker3D:
             if max_angle < 0.001:
                 break
 
-        vals, vecs, dH1 = s.find_twist()
-        self.dH1.append(np.linalg.norm(dH1))
-        self.dH2.append(vals)
-
-        chirality = s.get_chirality()
-        self.chirality.append(chirality)
-
-        fourier = s.get_fourier().cpu()
-        self.fourier = (self.fourier * self.n_realizations + fourier.numpy()) / (self.n_realizations + 1)
+        self.spin_inds.append(s.spin_inds_gl.data.cpu().numpy().copy())
+        self.thetas.append(s.thetas.data.cpu().numpy().copy())
+        self.phis.append(s.phis.data.cpu().numpy().copy())
 
         self.n_realizations += 1
         del s
@@ -57,31 +55,47 @@ class Worker3D:
 
         return True
 
+    @property
+    def n_undone_twists(self):
+        return self.n_realizations - self.n_twists_done
+
+    def _find_twists(self):
+        for i in tqdm(range(start=self.n_twists_done, stop=self.n_realizations)):
+            s = TDSystem3D(L=self.L, H=self.L, c=self.c,
+                           spin_inds_gl=self.spin_inds[i],
+                           thetas=self.thetas[i],
+                           phis=self.phis[i])
+
+            vals, vecs, dH1 = s.find_twist()
+            self.dH1.append(np.linalg.norm(dH1))
+            self.dH2.append(vals)
+
+            chirality = s.get_chirality()
+            self.chirality.append(chirality)
+
+            self.n_twists_done += 1
+
     def save(self, filename):
-        np.savez(file=filename, L=self.L, H=self.H, c=self.c, n_realizations=self.n_realizations,
-                 dH1=self.dH1, dH2=self.dH2, chirality=self.chirality, fourier=self.fourier)
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
 
     @classmethod
     def load(cls, filename):
-        f = np.load(filename + '.npz')
-        new = Worker3D(L=int(f['L']), H=int(f['L']), c=float(f['c']))
-        new.n_realizations = int(f['n_realizations'])
-        new.dH1 = [*f['dH1']]
-        new.dH2 = [*f['dH2']]
-        new.chirality = [*f['chirality']]
-        new.fourier = f['fourier']
-        return new
+        with open(filename, 'rb') as f:
+            w = pickle.load(f)
+        return w
+
 
     def __add__(self, other: 'Worker3D') -> 'Worker3D':
-        if self.H != other.H or self.L != other.L or self.c != other.c:
+        if self.L != other.L or self.c != other.c:
             raise ValueError('Both workers must have same dimensions and concentration.')
 
-        new: Worker3D = Worker3D(H=self.H, L=self.L, c=self.c)
+        new: Worker3D = Worker3D(L=self.L, c=self.c)
         new.n_realizations = self.n_realizations + other.n_realizations
         new.dH1 = self.dH1 + other.dH1
         new.dH2 = self.dH2 + other.dH2
         new.chirality = self.chirality + other.chirality
-        new.fourier = (self.n_realizations * self.fourier + other.n_realizations * other.fourier) / new.n_realizations
+
         return new
 
     def do(self, n: Optional[int] = None, save_every: Optional[int] = None,
@@ -93,14 +107,14 @@ class Worker3D:
         if save_every is None:
             save_every = n + 1
 
-        filename = f'{self.H}_{self.L}_{self.c}'
+        filename = f'{self.L}_{self.c}'
         if prefix is not None:
             filename = f'{prefix}_{filename}'
         if folder is not None:
             filename = f'{folder}/{filename}'
 
         for i in tqdm(count(1), total=n, desc='Number of realizations', ncols=120):
-            while not self._make_one_realization(device=device):
+            while not self._find_one_realization(device=device):
                 pass
 
             if i % save_every == 0:
@@ -125,3 +139,14 @@ class Worker3D:
         #     r.append(np.random.choice(x[:, 0], size=(self.n_realizations, n_dist), replace=True).mean(axis=-1))
 
         return y
+
+
+def collect(L, cs: List[float], prefixes: List[str], new_prefix: str):
+    for c in cs:
+        w = Worker3D(L, c)
+        for prefix in prefixes:
+            try:
+                w = w + Worker3D.load(f'{prefix}_{L}_{L}_{c}')
+            except:
+                pass
+        w.save(f'{new_prefix}_{L}_{L}_{c}')
